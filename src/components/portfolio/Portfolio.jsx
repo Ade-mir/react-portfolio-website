dfd94229c493f2bf7e7f983e15fcd696b886b30e
A.A <115159286+Ade-mir@users.noreply.github.com>
2023-02-28 18:12:57 +0100

    Deployed production build

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/Yoga-App-Promo.png";
+import IMG1 from "../../assets/brights-lite.png";
 
 const data = [
   {
     id: 1,
     image: IMG1,
     title: "MERN Stack Social Media Platform",
-    github: "https://github.com",
+    github: "https://github.com/Ade-mir/Brights-Lite",
     demo: "https://www.canva.com/design/DAFapZZx0GI/205p6bLMxmlp8Z4OjkvcMw/view?utm_content=DAFapZZx0GI&utm_campaign=designshare&utm_medium=link&utm_source=homepage_design_menu",
   },
 ];
